c6c74b2c4202eb5de50c0d87388657372d402c11
hmtheboy154 <buingoc67@gmail.com>
2024-09-10 03:23:36 -0400

    portable_fopencookie: use off_t on seek
    Fix the following error:
    external/efivar/src/error.c:255:11: error: incompatible function pointer types initializing 'int (*)(void *, off64_t, int)' (aka 'int (*)(void *, long, int)') with an expression of type 'int (void *, off_t *, int)' (aka 'int (void *, long *, int)') [-Werror,-Wincompatible-function-pointer-types]
                    .seek = dbglog_seek,
                            ^~~~~~~~~~~
    1 error generated.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- src/portable_fopencookie.h
+++ src/portable_fopencookie.h
@@ -20,7 +20,7 @@
 typedef struct cookie_io_functions_t {
     ssize_t (*read)(void *, char *, size_t);
     ssize_t (*write)(void *, const char *, size_t);
-    int (*seek)(void *, off64_t, int);
+    int (*seek)(void *, off_t *, int);
     int (*close)(void *);
 } cookie_io_functions_t;
 
